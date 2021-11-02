5e4049418f6ca1f6415b10ddfa17e2293c765c4f
HernanPetroni38 <hernan.petroni@hotmail.com>
2021-11-02 19:09:59 -0300

    Subiendo la actividad obligatoria

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        String nombre = lectura.next();
+
+        System.out.println("Ingrese su apellido");
+        String lastname = lectura.next();
+
+        System.out.println("Ingrese su edad");
+        int age = lectura.nextInt();
+
+        System.out.println("Ingrese un hobbie");
+        String hobbie = lectura.next();
+
+        System.out.println("Ingrese su editor de codigo preferido");
+        String edcod = lectura.next();
+
+        System.out.print("Ingrese el sistema operativo que utiliza habitualmente\n");
+        String sistop = lectura.next();
+
+        System.out.println("Usted ingresó: " + nombre + " " + lastname + " " +age+ " " +hobbie+ " " +edcod+" " + sistop);
+
+    }
+}
